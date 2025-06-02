7f38c4024fc918ef591d20fc2f1e30a8f1e46692
rupajosyula28 <rupajosyula28@gmail.com>
2025-06-02 18:15:55 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String[] args) {
+for (int i = 10; i<=50; i++){
+// used for loop to print number from 10 to 50
+    System.out.println(i);
+}
+    }
+}
---
+++ whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args) {
+int i = 10;
+while (i <=50){
+    System.out.println(i);
+    i++;
+}
+}
+    }
+
